40cb840cb2948fe1c57d30e76d8f87c1a7ca984c
ChangHangeol <hgchang1@naver.com>
2022-07-13 09:31:12 +0900

    docs : 프론트 폴더 구조 수정 및 문서명 카멜케이스 수정

--- frontend/components/Mainframe.js
+++ frontend/components/Mainframe.js
@@ -1,9 +1,9 @@
-import Headernav from "./common/headernav";
+import HeaderNav from "./common/HeaderNav";
 
 export default function Mainframe({children}) {
     return (
         <>
-        <Headernav />
+        <HeaderNav />
         {/* 이 밑의 자식을 room, mainpage로 바꿔가면서 동작하기 */}
         <div>
             {children}
