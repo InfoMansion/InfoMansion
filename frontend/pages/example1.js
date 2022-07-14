2570b2c720f61abacb6688172cec2c81772379ee
ChangHangeol <hgchang1@naver.com>
2022-07-14 23:20:53 +0900

    docs : 에셋 추가 및 방 예시 배치 구현

--- frontend/pages/example1.js
+++ frontend/pages/example1.js
@@ -1,9 +1,15 @@
+import { OrthographicCamera } from '@react-three/drei'
 import {Canvas, useThree, useFrame} from '@react-three/fiber'
 import { useEffect } from 'react'
 import { Camera } from 'three'
 // import Exasset from './roompage/exasset';
 import styles from '../styles/Home.module.css'
-import Map_ini from './roompage/map_ini/Map_ini'
+import Map_ini from './roompage/Map_ini'
+import Desk_a from './roompage/Desk'
+import Stand_small from './roompage/Stand_small'
+import Sprokit from './roompage/Sprokit'
+import Barrel from './roompage/Barrel'
+import Sofa_small from './roompage/Sofa_small'
 
 export default function Example() {
     
@@ -15,13 +21,15 @@ export default function Example() {
             state.camera.position.y = distance;
             state.camera.position.z = distance;
             
-            const degree = 45;
-            const anglex = - degree * Math.PI / 180;
-            const angley = degree * Math.PI / 180;
-
-            state.camera.rotation.x = -0.7;
-            state.camera.rotation.z = 0.6;
-            state.camera.rotation.y = angley;
+            
+            state.camera.lookAt(0, 0, 0);
+            
+            // const degree = 45;
+            // const anglex = - degree * Math.PI / 180;
+            // const angley = degree * Math.PI / 180;
+            // state.camera.rotation.x = -0.7;
+            // state.camera.rotation.z = 0.6;
+            // state.camera.rotation.y = angley;
         })
         return null
       }
@@ -31,16 +39,24 @@ export default function Example() {
             <div 
                 className={styles.container}
                 style={{ width : "500px", height : "500px" }}>
-            <Canvas 
-            >
+            <Canvas onCreated={state => state.gl.setClearColor("white")} >
                 {/* light */}
-                <pointLight position={[10, 10, 10]} intensity={3}/>
+                <pointLight position={[10, 10, -10]} intensity={0.6}/>
+                <pointLight position={[10, -10, -10]} intensity={0.8}/>
+                <pointLight position={[10, 10, 10]} intensity={0.4}/>
                 <ambientLight />
 
+                {/* camera */}
+                <OrthographicCamera makeDefault zoom={130} />
                 <RoomCamera />
-                {/* object */}
-                <Map_ini rotation={[0,0,0]} scale={1.0} />
 
+                {/* object */}
+                <Map_ini position={[0, 0, 0]} rotation={[0,0,0]} scale={1.0} />
+                {/* <Sprokit position={[0,0,0]} scale={10}/> */}
+                <Stand_small scale={10}/>
+                <Desk_a position={[1.4,0,1.75]} rotation={[0,1.57,0]} scale={0.8}/>
+                {/* <Barrel /> */}
+                <Sofa_small position={[0.45,0,0.4]} rotation={[0,-1.57,0]} scale={0.7}/>
 
             </Canvas>
 
