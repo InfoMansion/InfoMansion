677de471c9f825df66e9d28bb1f953b40e02af82
tbqlsmm <qldtn8@naver.com>
2022-07-15 17:55:22 +0900

    feat : Stuff 도메인 추가 및 생성 테스트

---
+++ backend/src/main/java/com/infomansion/server/domain/stuff/domain/Stuff.java
@@ -0,0 +1,42 @@
+package com.infomansion.server.domain.stuff.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Stuff {
+
+    @Id @GeneratedValue
+    @Column(name = "stuff_id")
+    private Long id;
+
+    private String stuffName;
+    private Long price;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    private int stuffSize;
+
+    @Enumerated(EnumType.STRING)
+    private StuffType stuffType;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "stuff_image_id")
+    private StuffImage stuffImage;
+
+
+    @Builder
+    public Stuff(String stuffName, Long price, Category category, int stuffSize, StuffType stuffType) {
+        this.stuffName = stuffName;
+        this.price = price;
+        this.category = category;
+        this.stuffSize = stuffSize;
+        this.stuffType = stuffType;
+    }
+}
---
+++ backend/src/test/java/com/infomansion/server/domain/stuff/repository/StuffRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.infomansion.server.domain.stuff.repository;
+
+import com.infomansion.server.domain.stuff.domain.Category;
+import com.infomansion.server.domain.stuff.domain.Stuff;
+import com.infomansion.server.domain.stuff.domain.StuffType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Transactional
+@SpringBootTest
+public class StuffRepositoryTest {
+
+    @Autowired
+    private StuffRepository stuffRepository;
+
+    @DisplayName("Stuff 생성 성공")
+    @Test
+    public void Stuff_생성() {
+        String stuffName = "laptop";
+        Long price = 30L;
+        Category category = Category.IT;
+        int stuffSize = 2;
+        StuffType stuffType = StuffType.S;
+        Stuff stuff = Stuff.builder()
+                .stuffName(stuffName)
+                .price(price)
+                .category(category)
+                .stuffSize(stuffSize)
+                .stuffType(stuffType)
+                .build();
+
+        stuffRepository.save(stuff);
+
+        List<Stuff> stuffList = stuffRepository.findAll();
+        assertThat(stuffList.get(0).getStuffName()).isEqualTo(stuffName);
+    }
+}
