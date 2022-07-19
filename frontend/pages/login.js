44f2f52fa04e888821735ea6bc08a60d0f76478a
SSAFY <john5755@naver.com>
2022-07-19 11:28:53 +0900

    style-로그인창

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useState } from 'react';
-import { Button, Checkbox, FormControlLabel, Grid, Link, TextField, Typography} from '@mui/material';
+import { Button, Checkbox, FormControlLabel, Grid, Link, TextField, Typography, Card, CardMedia} from '@mui/material';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import CssBaseline from '@mui/material/CssBaseline';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Paper from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Axios from 'axios';
 import { useRouter } from 'next/router';
@@ -75,23 +76,39 @@ export default function LogIn() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
+
+<Grid container component="main" sx={{ height: '100vh' }}>
         <CssBaseline />
-        <Box
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
           sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            backgroundImage: 'url(https://source.unsplash.com/random)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
           }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Card>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <CardMedia
+            component="img"
+            image="/logo.png">
+            </CardMedia>
+            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
             <TextField
               margin="normal"
               required
@@ -126,23 +143,25 @@ export default function LogIn() {
               sx={{ mt: 3, mb: 2 }}
               disabled={!(inputPw.length > 5 && inputId.includes('@'))}
             >
-              Sign In
+              LOGIN
             </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href={`/change`} variant="body2">
-                  Forgot password?
-                </Link>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="/signup" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
               </Grid>
-              <Grid item>
-                <Link href={`/signup`} variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
+            </Box>
           </Box>
-        </Box>
-      </Container>
+          </Card>
+        </Grid>
+      </Grid>
     </ThemeProvider>
   );
 }
