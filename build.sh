e7c01edf4c9e84d116f37f925f60fd8a802f9bbe
chiyongs <tlscldyd@gmail.com>
2022-07-21 14:25:16 +0900

    setting : 최적화를 통한 version 업그레이드

--- build.sh
+++ build.sh
@@ -3,11 +3,14 @@ docker rm spring
 docker rm react
 
 cd frontend
-docker build --tag react:0.0.1 .
+
+docker build --tag react:0.0.2 .
 
 cd ../backend
+# ./gradlew clean bootBuildImage -Pprofile=dev --platform linux/amd64
+./gradlew clean build
 docker build --tag spring:0.0.1 .
 docker run --name spring -d -p 8080:8080 spring:0.0.1
 
 cd ../frontend
-docker run --name react -d -p 3000:3000 react:0.0.1
+docker run --name react -d -p 3000:3000 react:0.0.2
