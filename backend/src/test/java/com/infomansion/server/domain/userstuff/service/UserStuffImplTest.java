bff0507dc42cf3e5083b5f3933ae9c2bc35b2488
tbqlsmm <qldtn8@naver.com>
2022-07-21 18:00:26 +0900

    test : UserStuff 수정 테스트 완료

--- backend/src/test/java/com/infomansion/server/domain/userstuff/service/UserStuffImplTest.java
+++ backend/src/test/java/com/infomansion/server/domain/userstuff/service/UserStuffImplTest.java
@@ -1,13 +1,17 @@
 package com.infomansion.server.domain.userstuff.service;
 
+import com.infomansion.server.domain.category.Category;
 import com.infomansion.server.domain.stuff.dto.StuffRequestDto;
 import com.infomansion.server.domain.stuff.repository.StuffRepository;
 import com.infomansion.server.domain.user.domain.User;
 import com.infomansion.server.domain.user.repository.UserRepository;
 import com.infomansion.server.domain.userstuff.domain.UserStuff;
+import com.infomansion.server.domain.userstuff.dto.UserStuffIncludeRequestDto;
+import com.infomansion.server.domain.userstuff.dto.UserStuffModifyRequestDto;
 import com.infomansion.server.domain.userstuff.dto.UserStuffRequestDto;
 import com.infomansion.server.domain.userstuff.dto.UserStuffResponseDto;
 import com.infomansion.server.domain.userstuff.repository.UserStuffRepository;
+import com.infomansion.server.global.util.exception.CustomException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -117,4 +121,165 @@ public class UserStuffImplTest {
         List<UserStuffResponseDto> list = userStuffService.findAllUserStuff(userId);
         assertThat(list.size()).isEqualTo(5);
     }
+
+    @DisplayName("사용자가 보유하고 있는 Stuff 하나를 배치된 상태로 변경")
+    @Test
+    public void user_stuff_배치_상태로_변경() {
+        // given
+        Long userStuffId = 12L;
+        for(int i = 0; i < stuffIds.size(); i+=2) {
+            UserStuffRequestDto requestDto = UserStuffRequestDto.builder()
+                    .stuffId(stuffIds.get(i)).userId(userId).build();
+            userStuffId = userStuffService.saveUserStuff(requestDto);
+        }
+
+        // when
+        UserStuffIncludeRequestDto requestDto = UserStuffIncludeRequestDto.builder()
+                .id(userStuffId)
+                .category("IT")
+                .alias("Java 모음집")
+                .posX(1.1f).posY(0.5f).posZ(0.0f)
+                .rotX(0.0f).rotY(0.2f).rotZ(1.5f).build();
+        userStuffId = userStuffService.includeUserStuff(requestDto);
+
+        // then
+        Optional<UserStuff> findUS = userStuffRepository.findById(userStuffId);
+        assertThat(findUS.isEmpty()).isFalse();
+        assertThat(findUS.get().getSelected()).isTrue();
+        assertThat(findUS.get().getAlias()).isEqualTo("Java 모음집");
+    }
+
+    @DisplayName("이미 배치된 Stuff를 다시 배치된 상태로 변경할 경우 오류")
+    @Test
+    public void user_stuff_배치_상태로_변경_오류() {
+        // given
+        Long userStuffId = 12L;
+        for(int i = 0; i < stuffIds.size(); i+=2) {
+            UserStuffRequestDto requestDto = UserStuffRequestDto.builder()
+                    .stuffId(stuffIds.get(i)).userId(userId).build();
+            userStuffId = userStuffService.saveUserStuff(requestDto);
+        }
+
+        UserStuffIncludeRequestDto requestDto = UserStuffIncludeRequestDto.builder()
+                .id(userStuffId)
+                .category("IT")
+                .alias("Java 모음집")
+                .posX(1.1f).posY(0.5f).posZ(0.0f)
+                .rotX(0.0f).rotY(0.2f).rotZ(1.5f).build();
+        userStuffId = userStuffService.includeUserStuff(requestDto);
+
+        // when
+        UserStuffIncludeRequestDto usird = UserStuffIncludeRequestDto.builder()
+                .id(userStuffId)
+                .category("GAME")
+                .alias("게임 모음")
+                .posX(2.1f).posY(3.5f).posZ(0.9f)
+                .rotX(0.8f).rotY(1.2f).rotZ(1.0f).build();
+
+        // then
+        assertThatThrownBy(() -> {userStuffService.includeUserStuff(usird);})
+                .isInstanceOf(CustomException.class)
+                .extracting("errorCode").extracting("code").isEqualTo(40062);
+    }
+
+    @DisplayName("사용자가 보유하고 있는 Stuff 하나를 제외된 상태로 변경")
+    @Test
+    public void user_stuff_제외_상태로_변경() {
+        // given
+        Long userStuffId = 12L;
+        for(int i = 0; i < stuffIds.size(); i+=2) {
+            UserStuffRequestDto requestDto = UserStuffRequestDto.builder()
+                    .stuffId(stuffIds.get(i)).userId(userId).build();
+            userStuffId = userStuffService.saveUserStuff(requestDto);
+        }
+        UserStuffIncludeRequestDto requestDto = UserStuffIncludeRequestDto.builder()
+                .id(userStuffId)
+                .category("IT")
+                .alias("Java 모음집")
+                .posX(1.1f).posY(0.5f).posZ(0.0f)
+                .rotX(0.0f).rotY(0.2f).rotZ(1.5f).build();
+        userStuffId = userStuffService.includeUserStuff(requestDto);
+
+        // when
+        userStuffService.excludeUserStuff(userStuffId);
+
+        // then
+        Optional<UserStuff> findUS = userStuffRepository.findById(userStuffId);
+        assertThat(findUS.isEmpty()).isFalse();
+        assertThat(findUS.get().getSelected()).isFalse();
+        assertThat(findUS.get().getAlias()).isEqualTo("Java 모음집");
+    }
+
+    @DisplayName("이미 제외된 Stuff를 다시 제외된 상태로 변경 요청할 경우 오류")
+    @Test
+    public void user_stuff_제외_상태로_변경_오류() {
+        // given
+        UserStuffRequestDto requestDto = UserStuffRequestDto.builder()
+                .stuffId(stuffIds.get(0)).userId(userId).build();
+        Long userStuffId = userStuffService.saveUserStuff(requestDto);
+
+        // when, then
+        assertThatThrownBy(() -> {userStuffService.excludeUserStuff(userStuffId);})
+                .isInstanceOf(CustomException.class)
+                .extracting("errorCode").extracting("code").isEqualTo(40061);
+    }
+
+    @DisplayName("배치된 Stuff의 Alias나 Category 변경")
+    @Test
+    public void user_stuff_alias_category_변경() {
+        // given
+        UserStuffRequestDto requestDtoGiven = UserStuffRequestDto.builder()
+                .stuffId(stuffIds.get(0)).userId(userId).build();
+        Long userStuffId = userStuffService.saveUserStuff(requestDtoGiven);
+
+        UserStuffIncludeRequestDto requestDto = UserStuffIncludeRequestDto.builder()
+                .id(userStuffId)
+                .category("IT").alias("Java 모음집")
+                .posX(1.1f).posY(0.5f).posZ(0.0f)
+                .rotX(0.0f).rotY(0.2f).rotZ(1.5f).build();
+        userStuffService.includeUserStuff(requestDto);
+
+        // when
+        UserStuffModifyRequestDto modifyRequestDto = UserStuffModifyRequestDto.builder()
+                .id(userStuffId)
+                .category("GAME")
+                .alias("메이플 공략 모음")
+                .build();
+
+        userStuffService.modifyAliasAndCategory(modifyRequestDto);
+
+        // then
+        Optional<UserStuff> findUS = userStuffRepository.findById(userStuffId);
+        assertThat(findUS.isEmpty()).isFalse();
+        assertThat(findUS.get().getCategory()).isEqualTo(Category.GAME);
+        assertThat(findUS.get().getAlias()).isEqualTo("메이플 공략 모음");
+    }
+
+    @DisplayName("Alias, Category 수정 시 둘 다 미입력할 경우 오류 발생")
+    @Test
+    public void user_stuff_alias_category_변경_오류() {
+        // given
+        UserStuffRequestDto requestDtoGiven = UserStuffRequestDto.builder()
+                .stuffId(stuffIds.get(0)).userId(userId).build();
+        Long userStuffId = userStuffService.saveUserStuff(requestDtoGiven);
+
+        UserStuffIncludeRequestDto requestDto = UserStuffIncludeRequestDto.builder()
+                .id(userStuffId)
+                .category("IT").alias("Java 모음집")
+                .posX(1.1f).posY(0.5f).posZ(0.0f)
+                .rotX(0.0f).rotY(0.2f).rotZ(1.5f).build();
+        userStuffService.includeUserStuff(requestDto);
+
+        // when
+        UserStuffModifyRequestDto modifyRequestDto = UserStuffModifyRequestDto.builder()
+                .id(userStuffId)
+                .build();
+
+        // then
+        assertThatThrownBy(() -> {userStuffService.modifyAliasAndCategory(modifyRequestDto);})
+                .isInstanceOf(CustomException.class)
+                .extracting("errorCode").extracting("code").isEqualTo(40063);
+
+    }
+
 }
