2b4fba808175ccaea29c6944e9947a23796f31df
SSAFY <john5755@naver.com>
2022-07-24 01:43:51 +0900

    refactor: .then대신 async

--- frontend/components/common/TokenUpdate.js
+++ frontend/components/common/TokenUpdate.js
@@ -5,30 +5,32 @@ import moment from 'moment';
 
 export default function TokenUpdate() {
   const [token, setToken] = useRecoilState(tokenState);
+  const isToken = !!token;
+  const isValid = false;
 
-  if (token) {
+  if (isToken) {
     const expiredAt = this.$moment.utc(token['expiresAt']);
     const diffTime = this.$moment.duration(expiredAt.diff(this.$moment()));
-    if (diffTime < 10000) {
-      Axios({
-        url: 'http://localhost:8080/api/v1/auth/reissue',
-        method: 'post',
-        withCredentials: true,
-      }).then(res => {
+    const isValid = diffTime >= 10000;
+  }
+
+  if (!isValid) {
+    const getToken = async () => {
+      try {
+        const { res } = await Axios({
+          url: 'http://localhost:8080/api/v1/auth/reissue',
+          method: 'post',
+          withCredentials: true,
+        });
         const accessToken = res.data.accessToken;
         const expiresAt = res.data.expirestAt;
-        setToken({ accessToken: accessToken, expiresAt: expiresAt });
-      });
-    } else {
-      Axios({
-        url: 'http://localhost:8080/api/v1/auth/reissue',
-        method: 'post',
-        withCredentials: true,
-      }).then(res => {
-        const accessToken = res.data.accessToken;
-        const expiresAt = res.data.expirestAt;
-        setToken({ accessToken: accessToken, expiresAt: expiresAt });
-      });
-    }
+        const [cookie] = res.headers['set-cookie'];
+        cookies.set(JSON.stringify(cookie));
+        setToken({
+          accessToken: accessToken,
+          expiresAt: expiresAt,
+        });
+      } catch {}
+    };
   }
 }
