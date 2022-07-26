861239c941d612d5a3a31c4fb9b8b704ddfc1357
김가영 <rk28716@naver.com>
2022-07-26 17:44:49 +0900

    [FEATURE] Post Domain, 생성 구현 및 테스트

---
+++ backend/src/main/java/com/infomansion/server/domain/post/domain/Post.java
@@ -0,0 +1,46 @@
+package com.infomansion.server.domain.post.domain;
+
+import com.infomansion.server.domain.category.Category;
+import com.infomansion.server.domain.userstuff.domain.UserStuff;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Post {
+
+    @Id @GeneratedValue
+    @Column(name = "post_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_stuff_id")
+    private UserStuff userStuff;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @Enumerated(value = EnumType.STRING)
+    private Category category;
+
+    private Long likes;
+
+    private boolean deleteFlag;
+
+    @Builder
+    public Post(Long id, UserStuff userStuff, String title, String content) {
+        this.id = id;
+        this.userStuff = userStuff;
+        this.title = title;
+        this.content = content;
+        this.category = userStuff.getCategory();
+        this.likes = 0L;
+        this.deleteFlag = false;
+    }
+}
---
+++ backend/src/main/java/com/infomansion/server/domain/post/service/PostService.java
@@ -0,0 +1,9 @@
+package com.infomansion.server.domain.post.service;
+
+import com.infomansion.server.domain.post.dto.PostCreateRequestDto;
+
+public interface PostService {
+
+    Long createPost(PostCreateRequestDto requestDto);
+
+}
