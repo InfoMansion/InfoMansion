66dfc935fd1f5ecf121047071c69eaba6b21131d
신치용 <cldydtls@naver.com>
2022-07-29 18:58:36 +0900

    [SETTING] AWS S3 연동을 위한 Spring Cloud Starter AWS 의존성 추가 및 간단 파일 업로드 기능 추가

---
+++ backend/src/main/java/com/infomansion/server/domain/upload/api/S3ApiController.java
@@ -0,0 +1,29 @@
+package com.infomansion.server.domain.upload.api;
+
+import com.infomansion.server.domain.upload.service.S3Uploader;
+import com.infomansion.server.global.apispec.BasicResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RequiredArgsConstructor
+@RestController
+public class S3ApiController {
+
+    private final S3Uploader s3Uploader;
+
+    @PostMapping("/api/v1/image/profile/{userId}")
+    public ResponseEntity<? extends BasicResponse> updateUserImage(@RequestParam("images")MultipartFile multipartFile, @PathVariable("userId") Long userId) {
+        try {
+            s3Uploader.uploadFiles(multipartFile, "static");
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
---
+++ backend/src/main/java/com/infomansion/server/domain/upload/service/S3Uploader.java
@@ -0,0 +1,66 @@
+package com.infomansion.server.domain.upload.service;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Component
+public class S3Uploader {
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public String uploadFiles(MultipartFile multipartFile, String dirName) throws IOException {
+        File uploadFile = convert(multipartFile)
+                .orElseThrow(() -> new IllegalArgumentException("error: MultipartFile -> File convert fail"));
+        return upload(uploadFile, dirName);
+    }
+
+    private String upload(File uploadFile, String filePath) {
+        String fileName = filePath + "/" + UUID.randomUUID() + uploadFile.getName();
+        String uploadImageUrl = putS3(uploadFile, fileName);
+        removeNewFile(uploadFile);
+        return uploadImageUrl;
+    }
+
+    private String putS3(File uploadFile, String fileName) {
+        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(CannedAccessControlList.PublicRead));
+        return amazonS3Client.getUrl(bucket, fileName).toString();
+    }
+
+    private void removeNewFile(File targetFile) {
+        if (targetFile.delete()) {
+            System.out.println("File delete success");
+            return;
+        }
+        System.out.println("File delete fail");
+    }
+
+    private Optional<File> convert(MultipartFile file) throws IOException {
+        File convertFile = new File(System.getProperty("user.dir") + "/" + file.getOriginalFilename());
+        System.out.println(convertFile);
+        if(convertFile.createNewFile()) {
+            System.out.println("hi");
+            try (FileOutputStream fos = new FileOutputStream(convertFile)) {
+                fos.write(file.getBytes());
+            }
+            System.out.println("return option convertfile");
+            return Optional.of(convertFile);
+        }
+        System.out.println("return optional empty");
+        return Optional.empty();
+    }
+}
