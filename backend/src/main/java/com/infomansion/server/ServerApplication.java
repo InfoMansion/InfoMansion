12cad8427b9ad5e97c5dc8d07be0dc96be235c92
신치용 <cldydtls@naver.com>
2022-08-07 23:34:35 +0900

    [FIX] Spring boot server time zone을 Asia/Seoul로 변경

--- backend/src/main/java/com/infomansion/server/ServerApplication.java
+++ backend/src/main/java/com/infomansion/server/ServerApplication.java
@@ -3,9 +3,16 @@ package com.infomansion.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ServerApplication {
 
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
