9ae78cbbe68e37018357ee9af5a0068e03a9f387
박민석 <pms16011172@gmail.com>
2022-08-14 15:06:14 +0900

    [FIX] 회원가입 버튼 페이지 이동 에러 수정

--- frontend/components/login.js
+++ frontend/components/login.js
@@ -17,24 +17,25 @@ import { useRecoilState } from 'recoil';
 import { likeCateState } from '../state/likeCate';
 import { useCookies } from 'react-cookie';
 import useAuth from '../hooks/useAuth';
-import styles from '../styles/Hex.module.css'
-import loginImgLoc from './jsonData/loginImgLoc.json'
+import styles from '../styles/Hex.module.css';
+import loginImgLoc from './jsonData/loginImgLoc.json';
+import Router from 'next/router';
 
 const theme = createTheme({
-  palette : {
-    primary : {
-      light : '#ff7961',
-      main : '#ffffff',
-      dark : '#fc7a71',
+  palette: {
+    primary: {
+      light: '#ff7961',
+      main: '#ffffff',
+      dark: '#fc7a71',
       contrastText: 'white',
     },
 
-    buttonColor : {
-      light : '#ff7961',
-      main : '#fc7a71',
-      dark : '#fc0000',
-      contrastText : 'white'
-    }
+    buttonColor: {
+      light: '#ff7961',
+      main: '#fc7a71',
+      dark: '#fc0000',
+      contrastText: 'white',
+    },
   },
 });
 
@@ -52,22 +53,21 @@ export default function LogIn({ onSignIn }) {
 
   function handleBoxSize() {
     setBoxWidth(0.4 * window.innerWidth);
-    if(window.innerWidth < 1000) {
+    if (window.innerWidth < 1000) {
       setBoxHeight(0.4 * window.innerWidth);
-    }
-    else {
+    } else {
       setBoxHeight(400);
     }
   }
 
   useEffect(() => {
-    window.addEventListener("resize", handleBoxSize);
-  }, [])
+    window.addEventListener('resize', handleBoxSize);
+  }, []);
 
   function goSignUp(event) {
     event.preventDefault();
     setlikeCate('');
-    router.push('/user/category');
+    Router.push('/user/category');
   }
 
   function handleInput(event) {
@@ -101,163 +101,161 @@ export default function LogIn({ onSignIn }) {
   };
 
   return (
-      <ThemeProvider theme={theme}>
-        {loginImgLoc.map( bundle => (
-          <div
-            style={{
-              position : 'absolute',
-              left : bundle.left + "%",
-              right : bundle.right + "%",
-
-              top : bundle.top + "%",
-              bottom : bundle.bottom + "%",
-
-              width : boxWidth,
-              height : boxHeight,
-            }}
-          >
-            {bundle.data.map( ({left, right, top, bottom, size, opacity}) => (
-              <div
-                style={{
-                  position : 'absolute',
-                  left : left + "%",
-                  right : right + "%",
-                  top : top + "%",
-                  bottom : bottom + "%",
-                  width : boxHeight * size * 0.01,
-                }}
-              >
-                <img 
-                  src="/test.png" 
-                  className={styles.itemlogin}
-                  style={{width : '100%'}}
-                  />
-                <img 
-                  src="/image/whiteBox.png" 
-                  className={styles.itemlogin}
-                  style={{
-                    width : '100%',
-                    position : 'absolute',
-                    opacity : opacity * 0.01
-                  }}
-                />
-              </div>
-            ))}
-          </div>
-        ))}
-
-        <Container
+    <ThemeProvider theme={theme}>
+      {loginImgLoc.map(bundle => (
+        <div
           style={{
-            height : window.innerHeight,
-            display : 'flex',
-            alignItems : 'center',
-            justifyContent : 'center'
+            position: 'absolute',
+            left: bundle.left + '%',
+            right: bundle.right + '%',
+
+            top: bundle.top + '%',
+            bottom: bundle.bottom + '%',
+
+            width: boxWidth,
+            height: boxHeight,
           }}
         >
-          
+          {bundle.data.map(({ left, right, top, bottom, size, opacity }) => (
+            <div
+              style={{
+                position: 'absolute',
+                left: left + '%',
+                right: right + '%',
+                top: top + '%',
+                bottom: bottom + '%',
+                width: boxHeight * size * 0.01,
+              }}
+            >
+              <img
+                src="/test.png"
+                className={styles.itemlogin}
+                style={{ width: '100%' }}
+              />
+              <img
+                src="/image/whiteBox.png"
+                className={styles.itemlogin}
+                style={{
+                  width: '100%',
+                  position: 'absolute',
+                  opacity: opacity * 0.01,
+                }}
+              />
+            </div>
+          ))}
+        </div>
+      ))}
 
-          <Box
-            sx={{
-              mx: 4,
-            }}
-          >
-            <Box >
+      <Container
+        style={{
+          height: window.innerHeight,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Box
+          sx={{
+            mx: 4,
+          }}
+        >
+          <Box>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+              }}
+            >
+              <img src="/logo.png" style={{ width: '25%', height: '35%' }} />
+            </div>
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{
+                mt: 1,
+              }}
+            >
               <div
                 style={{
                   display: 'flex',
-                  justifyContent: 'center',
+                  flexDirection: 'column',
+                  alignItems: 'center',
                 }}
               >
-                <img
-                  src="/logo.png"
-                  style={{ width: '25%', height: '35%' }}
+                <TextField
+                  required
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                  value={inputId}
+                  onChange={handleInput}
+                  variant="outlined"
+                  color="primary"
+                  focused
+                  sx={{
+                    m: 1,
+                    width: '50%',
+                  }}
+                />
+                <TextField
+                  required
+                  name="password"
+                  label="Password"
+                  type="password"
+                  autoComplete="current-password"
+                  value={inputPw}
+                  onChange={handleInput}
+                  variant="outlined"
+                  color="primary"
+                  focused
+                  sx={{
+                    m: 1,
+                    width: '50%',
+                  }}
                 />
               </div>
-              <Box
-                component="form"
-                onSubmit={handleSubmit}
-                noValidate
-                sx={{
-                  mt: 1,
-                }}
-              >
-                <div
-                  style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
+              <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <Button
+                  variant="contained"
+                  color="buttonColor"
+                  sx={{
+                    m: 1,
+                    color: 'white',
+                  }}
+                  disabled={!(confirmId && confirmPw)}
+                  onClick={handleSubmit}
+                >
+                  LOGIN
+                </Button>
+              </div>
+              <Grid container justifyContent="center">
+                <Link
+                  href="#"
+                  variant="body2"
+                  onClick={goSignUp}
+                  sx={{
+                    mx: 3,
                   }}
                 >
-                  <TextField
-                    required
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                    autoFocus
-                    value={inputId}
-                    onChange={handleInput}
-                    variant="outlined"
-                    color="primary"
-                    focused
-                    sx={{
-                      m : 1,
-                      width : '50%'
-                    }}
-                  />
-                  <TextField
-                    required
-                    name="password"
-                    label="Password"
-                    type="password"
-                    autoComplete="current-password"
-                    value={inputPw}
-                    onChange={handleInput}
-                    variant="outlined"
-                    color="primary"
-                    focused
-                    sx={{
-                      m : 1,
-                      width : '50%'
-                    }}
-                  />
-                </div>
-                <div
-                  style={{ display: 'flex', justifyContent: 'center' }}
+                  Sign Up
+                </Link>
+                <Link
+                  href="/user/forgotPassword"
+                  variant="body2"
+                  sx={{
+                    mx: 3,
+                  }}
                 >
-                  <Button
-                    variant="contained"
-                    color="buttonColor"
-                    sx={{ 
-                      m : 1, 
-                      color: 'white' 
-                    }}
-                    disabled={!(confirmId && confirmPw)}
-                    onClick={handleSubmit}
-                  >
-                    LOGIN
-                  </Button>
-                </div>
-                  <Grid container justifyContent="center" >
-                  <Link href="#" variant="body2" onClick={goSignUp}
-                      sx={{
-                        mx : 3
-                      }}
-                  >
-                    Sign Up
-                  </Link>
-                  <Link href="/user/forgotPassword" variant="body2"
-                    sx={{
-                      mx : 3
-                    }}
-                  >
-                    Forgot password?
-                  </Link>
-                </Grid>
-              </Box>
+                  Forgot password?
+                </Link>
+              </Grid>
             </Box>
           </Box>
-        </Container>
-      </ThemeProvider>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
