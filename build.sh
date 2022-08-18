5e5322baed294a573287a3e18182a2640c27a704
신치용 <cldydtls@naver.com>
2022-08-19 01:15:47 +0900

    [FIX] 개발용 docker build 시 최신 redis 이미지 사용

--- build.sh
+++ build.sh
@@ -8,7 +8,6 @@ cd mariadb
 
 echo "[BUILD] MariaDB"
 docker build --tag mariadb:0.0.1 .
-# docker run --name mariadb -d -p 3309:3306 mariadb:0.0.1 --restart=always
 
 echo "[RUN] MariaDB"
 
@@ -16,7 +15,6 @@ docker run \
     --name mariadb \
     -d \
     -p 3309:3306 \
-    --restart=always \
     -e MYSQL_ROOT_PASSWORD=root \
     -v mariadb-volume:/var/lib/mysql \
     mariadb:0.0.1
@@ -24,9 +22,8 @@ docker run \
 cd ../redis
 
 echo "[BUILD] Redis"
-docker build --tag redis:0.0.1 .
 echo "[RUN] Redis"
-docker run --name redis -d -p 6380:6379 -v redis-volume:/data redis:0.0.1
+docker run --name redis -d -p 6380:6379 -v redis-volume:/data redis
 
 cd ../frontend
 
@@ -37,8 +34,6 @@ echo "[RUN] React"
 docker run --name react -d -p 3000:3000 -e CI=true react:0.0.2
 
 cd ../backend
-./gradlew clean bootBuildImage -Pprofile=dev --platform linux/amd64
-
 echo "[BUILD] Spring"
 ./gradlew clean build
 docker build --tag spring:0.0.1 .
